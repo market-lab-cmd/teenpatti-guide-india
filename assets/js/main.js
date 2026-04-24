0041526f19db09f9ebfa881808f90f545f3b72c1
qq <gh818.zhou@gmail.com>
2026-04-24 22:15:00 +0800

    docs: 更新内容

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,4 +140,19 @@
     };
   }
 
+  // Google Analytics gtag.js
+  var GA_ID = 'G-T5PPQX5BDS';
+
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_ID;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function () {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag('js', new Date());
+  window.gtag('config', GA_ID);
 })();
